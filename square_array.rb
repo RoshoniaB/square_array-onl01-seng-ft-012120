eaee4d33f25c687d0e1796621fe6d7ab3192eac5
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-01-30 19:34:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
+  #create blank array
   squared = []
+  # loop through array that came as an argument
+  # shovel each element into the blank array created earlier
   array.each { |number| squared << number ** 2 }
+  #return the array, after it's been filled
   squared
 end
